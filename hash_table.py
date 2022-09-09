82f8f20e58f74fdee7d898f030b4dd98e6cbf5d8
Emerson Joaquim Tito Albino <ealbino@c2r8s1.42luanda.com>
2022-09-09 20:38:29 +0100

    feat(insert): add an insert function that enable insertion of str value into the hash_table

--- hash_table.py
+++ hash_table.py
@@ -8,6 +8,12 @@ class Hash:
             hash_code += ord(char)
         return hash_code % 10
 
+    def insert(self, value):
+        try:
+            index = self.hash_function(value)
+            self.safe.insert(index, value)
+        except TypeError:
+            print("invalid type, inform a str value")
+
 if __name__ == "__main__":
     obj = Hash()
-    print("{}".format(obj.hash_function("Bob")))
